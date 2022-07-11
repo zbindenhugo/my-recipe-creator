e764eefbd1abc0b5899da77999ff9a91d2281520
hugo.zbinden@autajon.com <Zbindenh_Autajon>
2022-07-11 17:33:51 +0200

    Refronte total car pas content du design

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,18 +1,58 @@
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 import './Home.css'
 
 function Home () {
-    /*return (
-        <>
-            <Container style={{textAlign: 'center'}}>
-                <Typography variant="h2" gutterBottom component="div">
-                    Mon créateur de recette
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom component="div">
-                    Application web permettant de créer vos propres recettes. <br /> Fini le papier, fini de demander à votre mère la recette, créez la, et gardez la pour toujours !
-                </Typography>
+    return (
+        <Container fluid style={{textAlign: 'center'}}>
+            <Container>
+                <h1 className="display-1 animate__animated animate__backInRight">CRECIPE</h1>
+                <small className='text-muted lead animate__animated animate__fadeIn animate__delay-1s'>Votre créateur de recette en ligne. Moins de papier, plus d'imagination !</small>
+            </Container> 
+            <Container className='animate__animated animate__backInLeft animate__delay-2s home-text-container' fluid>
+                <Row>
+                    <Col>
+                        <h6 className="display-6">Crecipe c'est quoi ?</h6>
+                    </Col>
+                    <Col>
+                        <Col><h6 className="display-6">Mais pourquoi créer Crecipe ?</h6></Col>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p className='home-simpletext'>Crecipe c'est une idée simple, qui nous servirai au quotidien : <br /><strong>créer ses propres recette</strong></p>
+                        <p className='home-simpletext'>Fini de re-demander encore une fois la recette à quelqu'un</p>
+                        <p className='home-simpletext'>Stop aux feuilles qui ont servis a écrire une recette et qui se perdent quand on range</p>
+                        <p className='home-simpletext'>Crecipe (Create Recipe : Créer recette en anglais) vous permettra de créer vos recette, et de les garder ...</p>
+                        <p className='home-simpletext'><strong>A VIE</strong></p>
+                        <p>(Plûtot cool non ?)</p>
+                    </Col>
+                    <Col>
+                        <p className='home-simpletext'>Je suis un jeune web développeur, qui, pour se perfectionner voulais avoir un projet concret.</p>
+                        <p className='home-simpletext'>J'adore également cuisiner, mais je ne note jamais mes recettes... c'est un jour où je demandais une recette à ma mère que l'idée m'est venu !</p>
+                        <p className='home-simpletext'>C'est un jour où je demandais une recette à ma mère que l'idée m'est venu !</p>
+                        <p className='home-simpletext'>
+                            <figcaption class="blockquote-footer">
+                                Hugo, 2022 : "Et si je faisais un créateur de recette pour tout les gens tête en l'air comme moi ?"
+                            </figcaption>
+                        </p>
+                        <p className='home-simpletext'>Et voilà comment est né le projet !</p>
+                    </Col>
+                </Row>
             </Container>
-        </>
-    )*/
+            <Container fluid>
+                <Navbar fixed='bottom'>
+                    <Container fluid id='navbar-container'>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Item>bidule</Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                </Container>
+        </Container>
+    )
 }
 
 export default Home;
--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -11,14 +11,26 @@ function Navigation ({isLoggedIn, openCloseLoginModal, loggedUser, handleDisconn
     <Container fluid>
       <Navbar sticky='top'>
         <Container fluid id='navbar-container'>
-          <Navbar.Brand>Crecipe</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to='/' active={location.pathname === '/' ? true : false}>Accueil</Nav.Link>
-              <Nav.Link as={Link} to='/recipes' active={location.pathname === '/recipes' ? true : false}>Toutes les recettes</Nav.Link>
-              <Nav.Link as={Link} to='/recipes/myrecipes' active={location.pathname === '/recipes/myrecipes' ? true : false}>Mes recettes</Nav.Link>
-              <Nav.Link as={Link} to='/recipes/createmyrepice' active={location.pathname === '/recipes/createmyrepice' ? true : false}>Créer une recette</Nav.Link>
+              <Nav.Link as={Link} to='/' active={location.pathname === '/' ? true : false}>
+                  Accueil
+              </Nav.Link>
+              
+              <Nav.Link as={Link} to='/recipes' active={location.pathname === '/recipes' ? true : false}>
+                Toutes les recettes
+              </Nav.Link>
+
+              <Nav.Link as={Link} to='/recipes/myrecipes' active={location.pathname === '/recipes/myrecipes' ? true : false} 
+                disabled={!isLoggedIn  ? true : false}>
+                  Mes recettes
+              </Nav.Link>
+
+              <Nav.Link as={Link} to='/recipes/createmyrepice' active={location.pathname === '/recipes/createmyrepice' ? true : false} 
+                disabled={!isLoggedIn ? true : false}>
+                  Créer une recette
+              </Nav.Link>
             </Nav>
             <Navbar.Collapse className="justify-content-end">
               <Navbar.Text>
@@ -32,7 +44,6 @@ function Navigation ({isLoggedIn, openCloseLoginModal, loggedUser, handleDisconn
                     {loggedUser[0].first_name} {loggedUser[0].last_name}
                     <Button id='navbar-buttons' onClick={handleDisconnectClick}>
                       <img src='https://cdn-icons-png.flaticon.com/512/1828/1828479.png' width={25} height={25} alt='deconnecticon'/> 
-                      
                       {' Déconnexion'}
                     </Button>
                   </>
